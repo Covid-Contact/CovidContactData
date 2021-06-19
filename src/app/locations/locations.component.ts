24cc44c454aa6e665cd217f33409a65528627dcb
Albert Pinto <apintogil@gmail.com>
2021-06-19 13:15:32 +0200

    Add location ui.

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ChartDataSets, ChartOptions, ChartType} from "chart.js";
+import {Label} from "ng2-charts";
 
 @Component({
   selector: 'app-locations',
@@ -6,6 +8,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./locations.component.css']
 })
 export class LocationsComponent implements OnInit {
+  location: String = ''
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public colors: Array<any> = [
+    {
+      backgroundColor: 'rgb(102, 187, 106)',
+    }
+  ]
+
+  public barChartData: ChartDataSets[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Interactions'}
+  ];
 
   constructor() {
   }
@@ -13,4 +32,11 @@ export class LocationsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onLocationChanged() {
+
+  }
+
+  onFilterStatistics() {
+
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -43,7 +43,6 @@ export class UsersComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
   }
 
   onAgeChanged() {
