f2c1108eddc0cc6590c5f3779e11d20204157d7c
Albert Pinto <apintogil@gmail.com>
2021-06-19 12:16:41 +0200

    Add button to screen.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import {MatButtonModule} from "@angular/material/button";
 import {UsersComponent} from './users/users.component';
 import {LocationsComponent} from './locations/locations.component';
 import {ChartsModule} from "ng2-charts";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -26,7 +31,13 @@ import {ChartsModule} from "ng2-charts";
     MatIconModule,
     MatToolbarModule,
     MatButtonModule,
-    ChartsModule
+    ChartsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ChartDataSets, ChartOptions, ChartType} from 'chart.js';
 import {Label} from 'ng2-charts';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-users',
@@ -8,42 +9,39 @@ import {Label} from 'ng2-charts';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  @Input() fromAge = undefined
+  @Input() toAge = undefined
+  @Input() gender = 'none'
+
+  public buttonColor = 'primary'
+
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
   public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
+  public colors: Array<any> = [
+    {
+      backgroundColor: 'rgb(102, 187, 106)',
+    }
+  ]
 
   public barChartData: ChartDataSets[] = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Interactions'}
   ];
+
+  public fromAgeControl = new FormControl(this.fromAge, [
+    Validators.min(1)
+  ])
+
+  public toAgeControl = new FormControl(this.toAge, [
+    Validators.min(1)
+  ])
 
   constructor() {
   }
 
   ngOnInit(): void {
-  }
-
-  // events
-  public chartClicked({event, active}: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
-
-  public chartHovered({event, active}: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
-
-  public randomize(): void {
-    // Only Change 3 values
-    this.barChartData[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      (Math.random() * 100),
-      56,
-      (Math.random() * 100),
-      40];
   }
 }
